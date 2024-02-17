54d68056d2fba5917bf1a235d3370112cdb42854
andlukass <lopess.andrade@gmail.com>
2024-02-17 16:15:20 +0000

    edit new_canvas func

--- src/utils/new_canvas.c
+++ src/utils/new_canvas.c
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_new_canvas.c                                    :+:      :+:    :+:   */
+/*   new_canvas.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 16:19:20 by user              #+#    #+#             */
-/*   Updated: 2024/02/16 16:21:52 by user             ###   ########.fr       */
+/*   Updated: 2024/02/17 16:14:40 by user             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-void	new_canvas(t_image *canvas, void *mlx)
+void	new_canvas(t_image *canvas, void *mlx, int height, int width)
 {
-	canvas->id = mlx_new_image(mlx, WIN_WIDTH, WIN_HEIGTH);
+	canvas->id = mlx_new_image(mlx, width, height);
 	canvas->addr = mlx_get_data_addr(canvas->id,
 			&canvas->bits_per_pixel, &canvas->line_length,
 			&canvas->endian);
