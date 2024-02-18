f9cbfc25c637e08b287c19e1f72e32179d1f27b5
andlukass <lopess.andrade@gmail.com>
2024-02-18 16:16:49 +0000

    finish minimap organization

--- src/minimap/minimap.c
+++ src/minimap/minimap.c
@@ -6,13 +6,13 @@
 /*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 12:02:58 by user              #+#    #+#             */
-/*   Updated: 2024/02/18 15:13:19 by llopes-d         ###   ########.fr       */
+/*   Updated: 2024/02/18 16:16:08 by llopes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-static void	print_temp_obj(t_game *game, int y, int x, int color)
+static void	print_minimap_square(t_game *game, int x, int y, int color)
 {
 	int	temp_x;
 	int	final_y;
@@ -20,18 +20,22 @@ static void	print_temp_obj(t_game *game, int y, int x, int color)
 
 	final_y = y + MINIMAP_SCALE;
 	final_x = x + MINIMAP_SCALE;
-
 	while (y < final_y && (y >= 0 && y < MINIMAP_HEIGHT))
 	{
 		temp_x = x;
 		while (temp_x < final_x && (temp_x >= 0 && temp_x < MINIMAP_WIDTH))
-			put_pixel_canva(&game->scene, (temp_x++) + MINIMAP_MARGIN, y + MINIMAP_POSITIONY, color);
+			put_pixel_canva(&game->scene, \
+			(temp_x++) + MINIMAP_POSITIONX, \
+			y + MINIMAP_POSITIONY, \
+			color);
 		y++;
 	}
 }
 
 static void	print_minimap_walls(t_game *game)
 {
+	int	y_middle;
+	int	x_middle;
 	int	y;
 	int	x;
 
@@ -42,10 +46,13 @@ static void	print_minimap_walls(t_game *game)
 		x = 0;
 		while (game->map.map[y][x])
 		{
-			// print_temp_obj(game, ((y * MINIMAP_SCALE) - (game->player.position[Y] * MINIMAP_SCALE)),((x  * MINIMAP_SCALE) - (game->player.position[X] * MINIMAP_SCALE)), get_trgb(0, 255, 255, 255));
+			x_middle = (x * MINIMAP_SCALE) + (MINIMAP_WIDTH / 2);
+			y_middle = (y * MINIMAP_SCALE) + (MINIMAP_HEIGHT / 2);
 			if (is_floor(game->map.map[y][x]))
-				print_temp_obj(game, ((y * MINIMAP_SCALE) + (MINIMAP_HEIGHT / 2) - (game->player.position[Y] * MINIMAP_SCALE)),
-				((x * MINIMAP_SCALE) + (MINIMAP_WIDTH / 2) - (game->player.position[X] * MINIMAP_SCALE)), get_trgb(0, 230, 230 ,230));
+				print_minimap_square(game, \
+				x_middle - (game->player.position[X] * MINIMAP_SCALE), \
+				y_middle - (game->player.position[Y] * MINIMAP_SCALE), \
+				get_trgb(0, 230, 230, 230));
 			x++;
 		}
 		y++;
@@ -54,19 +61,20 @@ static void	print_minimap_walls(t_game *game)
 
 static void	print_minimap_player(t_game *game)
 {
-	print_temp_obj(game, MINIMAP_HEIGHT / 2, MINIMAP_WIDTH / 2, get_trgb(0, 255, 50, 50));
+	print_minimap_square(game, MINIMAP_WIDTH / 2,
+		MINIMAP_HEIGHT / 2, get_trgb(0, 255, 50, 50));
 }
 
-static void print_minimap_background(t_game *game)
+static void	print_minimap_background(t_game *game)
 {
-	int y;
-	int x;
+	int	y;
+	int	x;
 
 	y = MINIMAP_POSITIONY;
 	while (y <= MINIMAP_POSITIONY + MINIMAP_HEIGHT)
 	{
-		x = MINIMAP_MARGIN;
-		while (x < MINIMAP_WIDTH + MINIMAP_MARGIN)
+		x = MINIMAP_POSITIONX;
+		while (x < MINIMAP_POSITIONX + MINIMAP_WIDTH)
 		{
 			put_pixel_canva(&game->scene, x, y, get_trgb(0, 28, 28, 28));
 			x++;
