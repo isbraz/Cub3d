101253007c5ca06eeaa72c5259c4e3bed00f82f4
andlukass <lopess.andrade@gmail.com>
2024-03-15 13:25:24 +0000

    norminette view_line

--- src/minimap/draw_view_line.c
+++ src/minimap/draw_view_line.c
@@ -6,38 +6,55 @@
 /*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 12:39:24 by llopes-d          #+#    #+#             */
-/*   Updated: 2024/03/15 12:51:22 by llopes-d         ###   ########.fr       */
+/*   Updated: 2024/03/15 13:25:14 by llopes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-void draw_view_line(t_game *game, int x1, int y1)
+static void	update_pixel(int *pixel, int next)
+{
+	if (*pixel < next)
+		*pixel += 1;
+	else
+		*pixel += -1;
+}
+
+static int	is_wall(t_game *game, int pixelx, int pixely)
+{
+	int	curr;
+
+	curr = get_pixel_canva(&game->scene, pixelx, pixely);
+	if (curr != get_trgb(0, 230, 230, 230) && curr != get_trgb(0, 255, 50, 50))
+		return (1);
+	return (0);
+}
+
+void	draw_view_line(t_game *game, int finalX, int finalY)
 {
 	int	err;
-	int	e2;
-	int	x0;
-	int	y0;
+	int	temp;
+	int	pixelx;
+	int	pixely;
 
-	x0 = MINIMAP_POSITIONX + (MINIMAP_WIDTH / 2) + 2;
-	y0 = MINIMAP_POSITIONY + (MINIMAP_HEIGHT / 2) + 2;
-	err = abs(x1 - x0) - abs(y1 - y0);
-	while (x0 != x1 || y0 != y1)
+	pixelx = MINIMAP_POSITIONX + (MINIMAP_WIDTH / 2) + 2;
+	pixely = MINIMAP_POSITIONY + (MINIMAP_HEIGHT / 2) + 2;
+	err = abs(finalX - pixelx) - abs(finalY - pixely);
+	while (pixelx != finalX || pixely != finalY)
 	{
-		if (get_pixel_canva(&game->scene, x0, y0) != get_trgb(0, 230, 230, 230) && \
-				get_pixel_canva(&game->scene, x0, y0) != get_trgb(0, 255, 50, 50))
-				break;
-		put_pixel_canva(&game->scene, x0, y0, get_trgb(0, 255, 50, 50));
-		e2 = 2 * err;
-		if (e2 > -abs(y1 - y0))
+		if (is_wall(game, pixelx, pixely))
+			break ;
+		put_pixel_canva(&game->scene, pixelx, pixely, get_trgb(0, 255, 50, 50));
+		temp = 2 * err;
+		if (temp > -abs(finalY - pixely))
 		{
-				err -= abs(y1 - y0);
-				x0 += x0 < x1 ? 1 : -1;
+			err -= abs(finalY - pixely);
+			update_pixel(&pixelx, finalX);
 		}
-		if (e2 < abs(x1 - x0))
+		if (temp < abs(finalX - pixelx))
 		{
-				err += abs(x1 - x0);
-				y0 += y0 < y1 ? 1 : -1;
+			err += abs(finalX - pixelx);
+			update_pixel(&pixely, finalY);
 		}
 	}
 }
