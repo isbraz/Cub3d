d7b3e82193edea16fadd80841f412ca231393cef
andlukass <lopess.andrade@gmail.com>
2024-03-15 18:19:00 +0000

    fix texture init

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isbraz-d <isbraz-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 17:09:45 by user              #+#    #+#             */
-/*   Updated: 2024/03/15 11:52:59 by isbraz-d         ###   ########.fr       */
+/*   Updated: 2024/03/15 18:18:45 by llopes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,13 +51,17 @@ void	new_game(t_game *game)
 	game->mlx.mlx = mlx_init();
 	game->mlx.window = mlx_new_window(game->mlx.mlx, WIN_WIDTH, WIN_HEIGHT, "cub3D!");
 	new_canvas(&game->scene, game->mlx.mlx, WIN_HEIGHT, WIN_WIDTH);
+	game->wall_textures[0].id = mlx_xpm_file_to_image(game->mlx.mlx, \
+		"src/textures/wall1.xpm", &game->wall_textures[0].width, \
+		&game->wall_textures[0].height);
+	game->wall_textures[0].addr = mlx_get_data_addr(game->wall_textures[0].id, &game->wall_textures[0].bits_per_pixel, &game->wall_textures[0].line_length, &game->wall_textures[0].endian);
 	game->player.position[X] = game->map.spawn_pos[X];
 	game->player.position[Y] = game->map.spawn_pos[Y];
 	game->player.pa = ((PI / 2) * (dir == 'S')) + (PI * (dir == 'W')) + (((3 * PI) / 2) * (dir == 'N'));
 	game->player.pdx = cos(game->player.pa);
 	game->player.pdy = sin(game->player.pa);
-    game->player.planeX = ((-0.5) * (dir == 'S')) + ((0.5) * (dir == 'N'));
-    game->player.planeY = ((-0.5) * (dir == 'W')) + ((0.5) * (dir == 'E'));
+	game->player.planeX = ((-0.5) * (dir == 'S')) + ((0.5) * (dir == 'N'));
+	game->player.planeY = ((-0.5) * (dir == 'W')) + ((0.5) * (dir == 'E'));
 }
 
 int main(int argc, char *argv[])
