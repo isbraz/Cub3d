8c15a111b74b5c16d660822d4ddcb21af58492a8
andlukass <lopess.andrade@gmail.com>
2024-03-16 17:19:26 +0000

    add collison

--- src/scene/moves.c
+++ src/scene/moves.c
@@ -6,7 +6,7 @@
 /*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 10:43:16 by isbraz-d          #+#    #+#             */
-/*   Updated: 2024/03/15 18:26:41 by llopes-d         ###   ########.fr       */
+/*   Updated: 2024/03/16 15:25:37 by llopes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,26 +41,40 @@ void	move_vision(int key, t_game *game)
 	}
 }
 
+
+static int has_collison(t_game *game, double delta[2])
+{
+	double new_pos[2];
+	double new_delta[2];
+	int start = 42;
+
+	while (start >= -42)
+	{
+		rotate_vector(delta, new_delta, start);
+		new_pos[X] = game->player.position[X] + new_delta[X] * 0.5;
+		new_pos[Y] = game->player.position[Y] + new_delta[Y] * 0.5;
+		if (game->map.map[(int)new_pos[Y]][(int)new_pos[X]] == '1')
+			return (1);
+		start -= 10;
+	}
+	return (0);
+}
+
 void	move_player(int key, t_game *game)
 {
-	if (key == 119 && game->map.map[(int)(game->player.position[Y] + game->player.delta[Y] * SPEED)][(int)(game->player.position[X] + game->player.delta[X] * SPEED)] != '1')
-	{
-		game->player.position[X] += (game->player.delta[X] * SPEED);
-		game->player.position[Y] += (game->player.delta[Y] * SPEED);
-	}
-	if (key == 115 && game->map.map[(int)(game->player.position[Y] - game->player.delta[Y] * SPEED)][(int)(game->player.position[X] - game->player.delta[X] * SPEED)] != '1')
-	{
-		game->player.position[X] -= (game->player.delta[X] * SPEED);
-		game->player.position[Y] -= (game->player.delta[Y] * SPEED);
-	}
-	if (key == 97 && game->map.map[(int)(game->player.position[Y] - game->player.delta[X] * SPEED)][(int)(game->player.position[X] + game->player.delta[Y] * SPEED)] != '1')
-	{
-		game->player.position[X] += (game->player.delta[Y] * SPEED);
-		game->player.position[Y] += (-game->player.delta[X] * SPEED);
-	}
-	if (key == 100 && game->map.map[(int)(game->player.position[Y] + game->player.delta[X] * SPEED)][(int)(game->player.position[X] - game->player.delta[Y] * SPEED)] != '1')
-	{
-		game->player.position[X] += (-game->player.delta[Y] * SPEED);
-		game->player.position[Y] += (game->player.delta[X] * SPEED);
-	}
+	double new[2];
+	if (key != 119 && key != 115 && key != 97 && key != 100)
+		return ;
+	if (key == 119)
+		rotate_vector(game->player.delta, new, 0);
+	if (key == 115)
+		rotate_vector(game->player.delta, new, 180);
+	if (key == 97)
+		rotate_vector(game->player.delta, new, 270);
+	if (key == 100)
+		rotate_vector(game->player.delta, new, 90);
+	if (has_collison(game, new))
+		return ;
+	game->player.position[X] += (new[X] * SPEED);
+	game->player.position[Y] += (new[Y] * SPEED);
 }
