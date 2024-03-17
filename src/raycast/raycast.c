8792a21016cf7367661e918a47291bd17a6a31af
Ismael Braz <ismael.braz03@hotmail.com>
2024-03-17 15:22:28 +0000

    put all images

--- src/raycast/raycast.c
+++ src/raycast/raycast.c
@@ -3,55 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   raycast.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: isbraz-d <isbraz-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 11:25:55 by isbraz-d          #+#    #+#             */
-/*   Updated: 2024/03/16 17:15:38 by llopes-d         ###   ########.fr       */
+/*   Updated: 2024/03/17 15:16:22 by isbraz-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-// static void draw_line(t_game *game, int x0, int y0, int x1, int y1, int color) 
-// {
-//     int dx = abs(x1 - x0);
-//     int dy = abs(y1 - y0);
-//     int sx = x0 < x1 ? 1 : -1;
-//     int sy = y0 < y1 ? 1 : -1;
-//     int err = dx - dy;
-
-//     while (x0 != x1 || y0 != y1)
-//     { 
-// 		// if (get_pixel_canva(&game->scene, x0, y0) != get_trgb(0, 230, 230, 230) && \
-// 		// 	get_pixel_canva(&game->scene, x0, y0) != RAY_COLOR)
-// 		// 	break;
-// 		if (x0 >= WIN_WIDTH || y0 >= WIN_HEIGHT || y0 < 0 || x0 < 0)
-// 			break;
-//         put_pixel_canva(&game->scene, x0, y0, color);
-//         int e2 = 2 * err;
-//         if (e2 > -dy)
-//         {
-//             err -= dy;
-//             x0 += sx;
-//         }
-//         if (e2 < dx)
-//         {
-//             err += dx;
-//             y0 += sy;
-//         }
-//     }
-// }
-
 void	raycast(t_game *game)
 {
-	double	planeX = game->player.plane[X];
-	double	planeY = game->player.plane[Y];
+	int		c;
 
 	for (int x = 0; x < WIN_WIDTH; x++)
 	{
 		double cameraX = 2 * x / (double)WIN_HEIGHT - 1;
-		double raydirY = game->player.delta[Y] + planeY * cameraX;
-		double raydirX = game->player.delta[X] + planeX * cameraX;
+		double raydirY = game->player.delta[Y] + game->player.plane[Y] * cameraX;
+		double raydirX = game->player.delta[X] + game->player.plane[X] * cameraX;
 		//which box of the map we're in
 		int mapX = (int)game->player.position[X];
 		int mapY = (int)game->player.position[Y];
@@ -109,11 +78,28 @@ void	raycast(t_game *game)
           side = 1;
         }
         //Check if ray has hit a wall
-		if (game->map.map[mapY][mapX] == '1') 
+		if (game->map.map[mapY][mapX] == '1')
+		{
 			hit = 1;
+			// Determine the texture to use based on the direction of the wall hit
+     	   if (side == 0) // NS wall hit
+        	{
+            	if (raydirX > 0)
+            		c = 0; // North
+           		else
+            		c = 1; // South
+        	}
+        	else // EW wall hit
+        	{
+            	if (raydirY > 0)
+             		c = 2; // East
+           		else
+            		c = 3; // West
+       		}
+		} 	
       }
 
-		//Calculate distance projected on camera direction (Euclidean distance would give fisheye effect!) ***
+		//Calculate distance projected on camera direction (Euclidean distance would give fisheye effect!)
     	if(side == 0)
 			perpWallDist = (sideDistX - deltaDistX);
     	else
@@ -136,7 +122,7 @@ void	raycast(t_game *game)
 		else
 			wallX = game->player.position[X] + perpWallDist * raydirX;
 		wallX -= floor((wallX));
-		int texX = (int)(wallX * (double)(game->wall_textures[0].width));
+		int texX = (int)(wallX * (double)(game->wall_textures[c].width));
 		if(side == 0 && raydirX > 0)
 			texX = game->wall_textures[0].width - texX - 1;
 		if(side == 1 && raydirY < 0)
@@ -147,7 +133,7 @@ void	raycast(t_game *game)
 			int texY = ((d * game->wall_textures->height) / lineHeight) / 256;
 			if (texY <= -1 || texX <= -1)
 				return ;
-			int color = get_pixel_canva(&game->wall_textures[0], texX, texY);
+				int color = get_pixel_canva(&game->wall_textures[c], texX, texY);
 			if (side == 1)
 				color = (color >> 1) & 8355711;
 			put_pixel_canva(&game->scene, x, y, color);
