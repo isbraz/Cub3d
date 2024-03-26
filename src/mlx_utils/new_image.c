6fbe590db90f6bbbf09130d0cf00200aba2fca1b
andlukass <lopess.andrade@gmail.com>
2024-03-26 20:02:13 +0000

    norminette mlx_utils

--- src/mlx_utils/new_image.c
+++ src/mlx_utils/new_image.c
@@ -6,7 +6,7 @@
 /*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 19:46:21 by llopes-d          #+#    #+#             */
-/*   Updated: 2024/03/26 19:57:36 by llopes-d         ###   ########.fr       */
+/*   Updated: 2024/03/26 20:01:56 by llopes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 void	new_image(t_game *game, t_image *image, char *path)
 {
-	image->id = mlx_xpm_file_to_image(
-		game->mlx.mlx,
-		path,
-		&image->width,
-		&image->height
+	image->id = mlx_xpm_file_to_image(\
+		game->mlx.mlx, \
+		path, \
+		&image->width, \
+		&image->height \
 	);
 	if (!image->id)
 	{
 		printf("Error\nxpm error.\n");
 		ft_close(game);
 	}
-	image->addr = mlx_get_data_addr(
-		image->id,
-		&image->bits_per_pixel,
-		&image->line_length,
-		&image->endian
+	image->addr = mlx_get_data_addr(\
+		image->id, \
+		&image->bits_per_pixel, \
+		&image->line_length, \
+		&image->endian \
 	);
 }
