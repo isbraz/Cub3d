d56a5370c2b1ec8ae0376ac895486a51a3b1a9cb
andlukass <lopess.andrade@gmail.com>
2024-03-26 17:24:21 +0000

    fix transparency

--- src/mlx_utils/put_pixel.c
+++ src/mlx_utils/put_pixel.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   put_pixel.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isbraz-d <isbraz-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 12:09:20 by user              #+#    #+#             */
-/*   Updated: 2024/03/26 15:56:39 by isbraz-d         ###   ########.fr       */
+/*   Updated: 2024/03/26 17:23:51 by llopes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	put_pixel_canva(t_image *image, int x, int y, int pixel)
 {
 	char	*dst;
 
-	if (!pixel)
+	if (!pixel || pixel == -16777216)
 		return ;
 	dst = image->addr + (y * image->line_length + x
 			* (image->bits_per_pixel / 8));
