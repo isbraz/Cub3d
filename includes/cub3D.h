eb4d8486c0390402361767ea9e68d6674a0facfb
andlukass <lopess.andrade@gmail.com>
2024-03-29 15:37:18 +0000

    fix 42header

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user <user@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: llopes-d <llopes-d@student.42.fr>           +#+  +:+       +#+       */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 21:07:27 by llopes-d          #+#    #+#             */
 /*   Updated: 2024/03/29 15:28:22 by user             ###   ########.fr       */
