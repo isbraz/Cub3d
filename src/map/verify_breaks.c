403ccb0846c67da4dfecbe4f2d423507bc5c402d
andlukass <lopess.andrade@gmail.com>
2024-03-31 14:23:23 +0100

    change verify breaks function

--- src/map/verify_breaks.c
+++ src/map/verify_breaks.c
@@ -3,28 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   verify_breaks.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llopes-d <llopes-d@student.42.fr>           +#+  +:+       +#+       */
+/*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 12:23:36 by user              #+#    #+#             */
-/*   Updated: 2024/03/29 15:30:45 by user             ###   ########.fr       */
+/*   Updated: 2024/03/31 14:22:45 by llopes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3D.h>
-
-static int	is_line_empty(char *line)
-{
-	int	index;
-
-	index = 0;
-	while (line[index] && line[index] != '\n')
-	{
-		if (line[index] != ' ')
-			return (0);
-		index++;
-	}
-	return (1);
-}
 
 int	verify_breaks(t_map *map)
 {
@@ -34,7 +20,7 @@ int	verify_breaks(t_map *map)
 	if (index <= 0)
 		return (0);
 	while (map->input[++index])
-		if (map->input[index] == '\n' && is_line_empty(&map->input[index + 1]))
+		if (map->input[index] == '\n' && map->input[index + 1] == '\n')
 			return (0);
 	return (1);
 }
