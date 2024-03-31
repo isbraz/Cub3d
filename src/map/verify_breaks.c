5d1c5818c39fb5bf41cb1a8f06ddcd0bb73f5e29
andlukass <lopess.andrade@gmail.com>
2024-03-31 18:34:40 +0100

    rm -1

--- src/map/verify_breaks.c
+++ src/map/verify_breaks.c
@@ -6,7 +6,7 @@
 /*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 12:23:36 by user              #+#    #+#             */
-/*   Updated: 2024/03/31 14:22:45 by llopes-d         ###   ########.fr       */
+/*   Updated: 2024/03/31 18:32:44 by llopes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	verify_breaks(t_map *map)
 {
 	int	index;
 
-	index = map->map_start -1;
+	index = map->map_start;
 	if (index <= 0)
 		return (0);
 	while (map->input[++index])
