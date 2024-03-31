2d53cdd801758180ea1ce29b64cd8159b1c85a07
andlukass <lopess.andrade@gmail.com>
2024-03-31 14:10:57 +0100

    replace null call

--- src/utils/time_now.c
+++ src/utils/time_now.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   time_now.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llopes-d <llopes-d@student.42.fr>           +#+  +:+       +#+       */
+/*   By: llopes-d <llopes-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 19:43:16 by llopes-d          #+#    #+#             */
-/*   Updated: 2024/03/29 15:30:45 by user             ###   ########.fr       */
+/*   Updated: 2024/03/31 14:10:18 by llopes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ long	time_now(void)
 	long int		now;
 	struct timeval	time;
 
-	gettimeofday(&time, NULL);
+	gettimeofday(&time, 0);
 	now = (time.tv_sec * 1000 + time.tv_usec / 1000);
 	return (now);
 }
